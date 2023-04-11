183f66f75c1f1d99a5a1d920b272706d22f42cad
Travis Gomez <frank.travis.gomez@gmail.com>
2023-04-11 09:50:25 -0500

    contacts.txt

--- src/ContactsMngr.java
+++ src/ContactsMngr.java
@@ -4,23 +4,27 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ContactsMngr {
-    public static void main(String[] args) throws IOException {
-        String ourDirectory = "src/ContactsMngr/contacts";
-        Path pathToContacts = Path.of(ourDirectory);
-        System.out.println("pathToContacts = " + pathToContacts);
-        if(Files.notExists(pathToContacts)){
-            Files.createDirectory(pathToContacts);
-        }
-        String contactsFile = "contacts.txt";
-        Path directoryAndFile = Path.of(ourDirectory, contactsFile);
-        System.out.println("directoryAndFile = " + directoryAndFile);
-        if(Files.notExists(directoryAndFile)){
-            Files.createFile(directoryAndFile);
-        }
+    public static List<String> contactArray= new ArrayList<>();
+    public static int userMenu (){
+        System.out.println("Hello welcome to our contact manager. Please select a menu option between 1-5");
+        System.out.println("1) View contacts.");
+        System.out.println("2) Add contacts.");
+        System.out.println("3) Search contacts.");
+        System.out.println("4) Delete contacts.");
+        System.out.println("5) Exit.");
+        Scanner scanner = new Scanner (System.in);
+        int userInt = scanner.nextInt();
+        return userInt;
     }
+    public static void main(String[] args) throws IOException {
+
+    }
+
 }
